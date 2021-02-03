7042b1ec37730f83dc64ed2b79724121b2e31e68
FidelMunywoki <42610692+FidelMunywoki@users.noreply.github.com>
2021-02-03 22:26:21 +0300

    Update 4-puts.c
    Using puts to print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+
 /**
-*main-starts the code
-*returns int
-*/
+ *main - starts the code
+ *Description: prints "Programming is like building a multilingual puzzle
+ *Return: int
+ */
 
 int main(void)
 {
-
-  puts("\"Programming is like building a multilingual puzzle");
-
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }

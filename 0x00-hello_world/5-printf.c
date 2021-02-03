9e53a9c239f3a1708f9fe90916383be1019fc74a
FidelMunywoki <42610692+FidelMunywoki@users.noreply.github.com>
2021-02-03 22:21:08 +0300

    Update 5-printf.c
    using printf ttoprint

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
-int main (void)
+/**
+ * main - starts the code
+ * Description: prints with proper grammar, but the outcome is a piece of art
+ * Return: 0
+ */
 
+int main(void)
 {
-
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-
-  return (0);
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }

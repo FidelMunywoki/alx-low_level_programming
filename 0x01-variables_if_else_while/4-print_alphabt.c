b8d2a1460edd2ee7985acf68b6f9ad9a7517a8de
FidelMunywoki <fidelismunywoki12@gmail.com>
2021-02-04 14:19:25 +0000

    Print all the letters except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+cription: prints all alphabet except q and e
+* Return: 0
+ */
+int main(void)
+{
+char ch = 'a';
+
+while (ch <= 'z')
+{
+if (ch != 'e' && ch != 'q')
+{
+putchar(ch);
+}
+ch++;
+}
+putchar('\n');
+return (0);
+}

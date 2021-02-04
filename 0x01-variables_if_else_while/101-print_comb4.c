d92d7be362d6f9c9252d019e6341d688b1db6a75
FidelMunywoki <fidelismunywoki12@gmail.com>
2021-02-04 20:05:46 +0000

    advanced task

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - code execution
+ * Description: print possible combination of 3 digits
+ * Return: 0
+ */
+int main(void)
+{
+int i, j, k;
+
+for (i = 48; i < 58; i++)
+{
+for (j = i; j < 58; k++)
+{
+for (k = j; k < 58; k++)
+{
+if (i == j || j == k || i == k)
+{
+continue;
+}
+putchar(i);
+putchar(j);
+putchar(k);
+if (i == 55 && j == 56 && k == 57)
+{
+break;
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}

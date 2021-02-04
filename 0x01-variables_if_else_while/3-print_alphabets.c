b605308d80ac06d1a7a1dfcee95c9e02b83af369
FidelMunywoki <fidelismunywoki12@gmail.com>
2021-02-04 14:07:28 +0000

    Program to print a to z and A to Z

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+int main(void)
+
+{
+char x;
+
+char y;
+
+for (x = 'a'; x <= 'z'; x++)
+{putchar(x); }
+
+for (y = 'A'; y <= 'Z'; y++)
+{putchar(y); }
+
+return (0);
+}

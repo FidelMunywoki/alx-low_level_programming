33847abae7b34b644bc24f98c41b10a85d90a124
FidelMunywoki <fidelismunywoki12@gmail.com>
2021-02-10 15:01:38 +0000

    update

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,12 +1,10 @@
 #include "holberton.h"
-/*
- *HEAD
- *
- * print_times_table - writes a function that prints the n times table
- * @n: the n times table
-* print_times_table - writes a function that prints the n times table
-* @n: the n times table
-*/
+
+/**
+ * print_times_table - Print the `n` times table, starting with 0.
+ * Description: If `n` is greater than 15 or less than 0, print nothing.
+ * @n: int type number
+ */
 
 void print_times_table(int n)
 {

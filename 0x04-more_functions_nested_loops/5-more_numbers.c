93c8d7f745906b908aa6fb1bf50d87a0be8fd7fa
FidelMunywoki <fidelismunywoki12@gmail.com>
2021-02-11 20:33:03 +0000

    more on nested loops and functions

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * prints 10 times the numbers, from 0 to 14, followed by newline
+ * Description: You can only use _putchar
+ * Return: 0-14 ten times
+ */
+
+void more_numbers(void)
+{
+int c, i;
+
+c = 0;
+while (c < 10)
+{
+i = 0;
+while (i < 15)
+{
+if (i > 9)
+_putchar(i / 10 + '0');
+
+_putchar(i % 10 + '0');
+i++;
+}
+c++;
+_putchar('\n');
+}
+}

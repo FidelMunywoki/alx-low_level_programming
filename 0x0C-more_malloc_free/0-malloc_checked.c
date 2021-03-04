44d0be2ab87e23a66109787c548c87c62433c8b8
FidelMunywoki <fidelismunywoki12@gmail.com>
2021-03-04 20:47:57 +0000

    update on 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,30 +1,19 @@
 #include "holberton.h"
-#include <stdlib.h>
 
 /**
- *create_array - creates an array of chars and
- *               initializes with special char.
- *@size: Size of the array to be initialized.
- *@c: The spcific char to initialize the array with.
- *
- *Return: If size == 0 or the function fails -NULL.
- *        Otherwise - a pointer to the array.
+ * malloc_checked - allocates memory using malloc
+ * @b: unsigned integer
+ * Description: if malloc fails, normal process termination,status value 98
+ * Return: pointer to the allocated memory
  */
 
-char *create_array(unsigned int size, char c)
+void *malloc_checked(unsigned int b)
 {
-char *array;
-unsigned int i;
+void *a;
 
-if (size == 0)
-return (NULL);
+a = malloc(b);
+if (a == NULL)
+exit(98);
 
-array = malloc(sizeof(char) * size);
-
-if (array == NULL)
-return (NULL);
-
-for (i = 0; i < size; i++)
-array[i] = c;
-return (array);
+return (a);
 }

7409bcbfcaab9d6b19e49bb3bed4afbfcffae79e
FidelMunywoki <42610692+FidelMunywoki@users.noreply.github.com>
2021-03-05 08:47:32 +0300

    Update 3-array_range.c
    error fix

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,26 +1,31 @@
 #include "holberton.h"
+#include <stdlib.h>
 
 /**
- * array_range - creates an array of integers
- * @min: minimum val
- * @max: max value
- * Description: array contain all values from min-max, ordered
- * Return: pointer to the newly created array
+ *array_range - Create an array of integers
+ *@min: Minimum number of arrays included
+ *@max: Maximum number of arrays excluded
+ *
+ *Return: If min > max, malloc fails -NULL
+ *        Otherwise - The pointer to the newly created array
  */
 
 int *array_range(int min, int max)
 {
-int *a, i;
+	int *array, index, size;
 
-if (min > max)
-return (NULL);
+	if (min > max)
+		return (NULL);
 
-a = malloc((max - min + 1) * sizeof(*a));
-if (a == NULL)
-return (NULL);
+	size = max - min + 1;
 
-for (i == 0; min <= max; i++, min++)
-a[i] = min;
+	array = malloc(sizeof(int) * size);
 
-return (a);
+	if (array == NULL)
+		return (NULL);
+
+	for (index = 0; index < size; index++)
+		array[index] = min++;
+
+	return (array);
 }
